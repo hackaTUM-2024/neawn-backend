a158acf94c649cfdb63d4939cd49cd3e21031b0e
Pasquale Riello <pas.riello@gmail.com>
2024-11-23 23:44:18 +0100

    sync

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"log"
 	"neawn-backend/internal/app"
+	"runtime"
 )
 
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
 	app := app.New()
 	if err := app.Run(); err != nil {
 		log.Fatal(err)
