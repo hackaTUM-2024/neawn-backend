d3c823db73aeb5d6a31f2959b0e455c40b3f4e3a
Nicolò Battocchio <nicolo.battocchio2@gmail.com>
2024-11-23 11:39:46 +0100

    Squashed commit of the following:
    commit 7d65680465c5962e8b6c532a9d339b7c3c10212e
    Author: Nicolò Battocchio <nicolo.battocchio2@gmail.com>
    Date:   Sat Nov 23 11:02:00 2024 +0100
    
        postgres connection is now working
    
    commit 025232427cda854eec3f458ef7cb90abd25b1fba
    Author: Nicolò Battocchio <nicolo.battocchio2@gmail.com>
    Date:   Sat Nov 23 10:09:33 2024 +0100
    
        add db usage
    
    commit 7b5b97fb38bb8651f98593acc919453f057d7e1f
    Author: Nicolò Battocchio <nicolo.battocchio2@gmail.com>
    Date:   Sat Nov 23 10:05:07 2024 +0100
    
        add postgresql

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"log"
 	"neawn-backend/internal/app"
+	"neawn-backend/internal/db"
 )
 
 func main() {
+	db.Init()
+	defer db.Close()
+
 	app := app.New()
 	if err := app.Run(); err != nil {
 		log.Fatal(err)
