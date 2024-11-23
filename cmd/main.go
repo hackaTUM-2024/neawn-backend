d946593909df4c4410cf1ef5a07f0298ca245b65
Nicolò Battocchio <nicolo.battocchio2@gmail.com>
2024-11-23 16:16:22 +0100

    add working Redis sample

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"neawn-backend/internal/app"
 	"neawn-backend/internal/db"
+
+	"github.com/go-redis/redis"
 )
 
 func main() {
 	db.Init()
 	defer db.Close()
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379", // Redis server address
+		Password: "",               // No password set
+		DB:       0,                // Use default DB
+	})
+
+	err := rdb.Set("foo", "bar", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := rdb.Get("foo").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("foo", val)
+
+	defer rdb.Close()
 
 	app := app.New()
 	if err := app.Run(); err != nil {
